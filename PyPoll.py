446bb01810187d91bdff3128d1861d3f8fc95fa6
Yodit Teamir <79291410+Yoditatr@users.noreply.github.com>
2021-03-14 11:25:32 -0400

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -24,22 +24,22 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
-
     # Write some data to the file.
-    txt_file.write("Hello World")
-# Write three counties to the file.
-     txt_file.write("Arapahoe")
-     txt_file.write("Denver")
-     txt_file.write("Jefferson")
 
-# Write three counties to the file.
-     txt_file.write("Arapahoe")
-     txt_file.write("Denver")
-     txt_file.write("Jefferson")
-# Write three counties to the file.
-     txt_file.write("Arapahoe, Denver, Jefferson")
-# Write three counties to the file.
-     txt_file.write("Arapahoe\nDenver\nJefferson")
+    txt_file.write("Hello World")
+    # Write three counties to the file.
+    txt_file.write("Arapahoe")
+    txt_file.write("Denver")
+    txt_file.write("Jefferson")
+
+    # Write three counties to the file.
+    txt_file.write("Arapahoe")
+    txt_file.write("Denver")
+    txt_file.write("Jefferson")
+    # Write three counties to the file.
+    txt_file.write("Arapahoe, Denver, Jefferson")
+    # Write three counties to the file.
+    txt_file.write("Arapahoe\nDenver\nJefferson")
 
 
 
@@ -53,8 +53,9 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
+    
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
+# Read and print the header row.
     headers = next(file_reader)
     print(headers)
